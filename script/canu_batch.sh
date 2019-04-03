c1acad7139eeb306662c874e28f80e113fbf3cbc
Roberto Rossini <roberto.rossini.9533@student.uu.se>
2019-04-03 16:36:37 +0200

    Minor correction to canu script

--- script/canu_batch.sh
+++ script/canu_batch.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -l
 
+#SBATCH -A ***REMOVED***
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 12:00:00
+#SBATCH -J canu_l_ferriphilum_genome_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user robymetallo@users.noreply.github.com
+
 # This script assembles a genome with canu
 # using the reads from fastq.gz files
-
 
 IN_DIR="$HOME/prj/0_data/raw_data/DNA_raw_data"
 OUT_DIR="$HOME/prj/prj/0_daata/assembly"
@@ -16,14 +23,6 @@ CORR_ERR_RATE="0.040"                   # Improves assembly speed without affect
                                         # Setting taken from the doc
 IN_FILES="ERR2028*.fastq.gz"            # This expression will be expanded to all fastq.gz
                                         # files in a directory
-
-#SBATCH -A ***REMOVED***
-#SBATCH -p core
-#SBATCH -n 2
-#SBATCH -t 12:00:00
-#SBATCH -J canu_l_ferriphilum_genome_assembly
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user robymetallo@users.noreply.github.com
 
 canu -p $PREFIX genomeSize=$GENOME_SIZE \
 -d $OUT_DIR stopOnReadQuality=$STOP_ON_RQ \
