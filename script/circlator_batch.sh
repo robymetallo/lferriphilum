20d36a4ae487978ec88e60e98a9e8db05b8898bd
Roberto Rossini <roberto.rossini.9533@student.uu.se>
2019-04-07 22:59:16 +0200

    Fixed log path in circlator_batch.sh

--- script/circlator_batch.sh
+++ script/circlator_batch.sh
@@ -25,11 +25,10 @@ module load spades/3.12.0
 IN_DIR="$HOME/prj/analysis"
 OUT_DIR="$HOME/prj/data/DNA_data/circlator"
 CORR_READS="$HOME/prj/data/DNA_data/corrected_reads/LSP_ferriphilum.correctedReads.fasta.gz"
+LOG_PATH="$HOME/prj/data/DNA_data/circlator_batch_job_07-04-2019.log"
 
 # Settings:
 CONTIG_IN="LSP_ferriphilum_polished.contigs.fasta"
 
 # circlator v1.5.5
-command time -v circlator all --assembler canu $IN_DIR/$CONTIG_IN $CORR_READS $OUT_DIR 2>&1 | tee -a $OUT_DIR/circlator_shell.log
-
-sleep 30s && exit
+command time -v circlator all --assembler canu $IN_DIR/$CONTIG_IN $CORR_READS $OUT_DIR 2>&1 | tee -a $LOG_PATH
