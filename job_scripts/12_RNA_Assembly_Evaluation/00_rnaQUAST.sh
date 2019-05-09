a99e7ad8a02e400e945002042a0106e690d7bb7a
Roberto Rossini <roberto.rossini.9533@student.uu.se>
2019-05-09 13:02:20 +0200

    Removed unused scripts

---
+++ job_scripts/12_RNA_Assembly_Evaluation/00_rnaQUAST.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+IN_DIR="$HOME/Bioinformatics_data/lferriphilum/analysis/RNA/01_processed_reads/00_dedup"
+OUT_DIR="$HOME/Bioinformatics_data/lferriphilum/data/RNA_data/rnaQUAST"
+
+REF_GENOME="$HOME/Bioinformatics_data/lferriphilum/analysis/DNA/02_contigs/20_LFerr_genome_assembly_final.fasta"
+TRANS_ASSEMBLY="$HOME/Bioinformatics_data/lferriphilum/analysis/RNA/03_assembled_transcriptome/LFerr_Transcriptome_Assembly_Trinity.fasta"
+FWD_READS="LFerr_OK_1_merged_renamed.fastq.gz"
+REV_READS="LFerr_OK_2_merged_renamed.fastq.gz"
+
+mkdir -p $OUT_DIR
+
+/usr/bin/python2 \
+$HOME/Bioinformatics_tools/rnaQUAST-1.5.2/\
+rnaQUAST.py -r $REF_GENOME \
+            --prokaryote \
+            -c $TRANS_ASSEMBLY \
+            -1 $IN_DIR/$FWD_READS \
+            -2 $IN_DIR/$REV_READS \
+            -o $OUT_DIR \
+            --threads 4 \
+            2>&1 | tee $OUT_DIR/rnaQUAST_tee.log
---
+++ job_scripts/12_RNA_Assembly_Evaluation/01_BUSCO.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH -A ***REMOVED***
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 00:15:00
+#SBATCH --qos=short
+#SBATCH -J BUSCO_LFerr
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user robymetallo@users.noreply.github.com
+
+# Load modules
+module load bioinfo-tools
+module load BUSCO/3.0.2b
+
+OUT_DIR="$HOME/prj/data/RNA_data/BUSCO"
+
+TRANS_ASSEMBLY="$HOME/prj/analysis/RNA/03_assembled_transcriptome/LFerr_Transcriptome_Assembly_Trinity.fasta"
+PREFIX="LFerr"
+DB=$BUSCO_LINEAGE_SETS/bacteria_odb9
+
+
+mkdir -p $OUT_DIR
+
+python $BUSCO -i $TRANS_ASSEMBLY \
+                -o $PREFIX \
+                -l $DB \
+                -c 4 \
+                --mode tran \
+                --long \
+                --tarzip \
+                2>&1 | tee $OUT_DIR/BUSCO_tee.log
